6fa90069586107cdf1505dfaf60ff5c10d364250
marufkhan09 <maruf1609@gmail.com>
2023-11-16 01:12:14 +0600

    Initial Commit

---
+++ DiceApp-SwifUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DiceApp-SwifUI
+//
+//  Created by Maruf Khan on 16/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DiceApp-SwifUI/DiceApp_SwifUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceApp_SwifUIApp.swift
+//  DiceApp-SwifUI
+//
+//  Created by Maruf Khan on 16/11/23.
+//
+
+import SwiftUI
+
+@main
+struct DiceApp_SwifUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
