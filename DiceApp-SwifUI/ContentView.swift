da0c2f7fc8150d29e5474474a56117c00f3aab59
marufkhan09 <maruf1609@gmail.com>
2023-11-16 17:17:36 +0600

    updating a property with @state

--- DiceApp-SwifUI/ContentView.swift
+++ DiceApp-SwifUI/ContentView.swift
@@ -8,21 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+   @State var leftDiceNumber = 1
+   @State var rightDiceNumber = 1
     var body: some View {
         ZStack {
+            
             Image("background").resizable().edgesIgnoringSafeArea(.all).aspectRatio(contentMode: .fill)
+           
             VStack {
+                Spacer()
                 Image("diceeLogo")
                 Spacer()
                 HStack {
-                    DiceView(n: 1)
-                    DiceView(n: 2)
+                    DiceView(n: leftDiceNumber)
+                    DiceView(n: rightDiceNumber)
                    
                 }.padding(.all)
               Spacer()
                 Button(
                     
                     action: {
+                        
+                        leftDiceNumber = Int.random(in: 1...6)
+                        rightDiceNumber = Int.random(in: 1...6)
                     
                 }, label: {
                     Text("Roll")
